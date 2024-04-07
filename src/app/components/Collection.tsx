b1d09aa6a9938a5ab98b894060a00d4fe1b35c6c
unknown <yonideserbaix@gmail.com>
2024-04-07 13:45:57 +0200

    feat: import & render data collection

--- src/app/components/Collection.tsx
+++ src/app/components/Collection.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,7 +9,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { ShoppingBagIcon, ShoppingCart } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+import { CollectionData } from "./CollectionData";
 
 // Definition of types
 type ArticleType = {
@@ -28,33 +28,39 @@ export default async function Collection() {
       <h1 className="text-3xl lg:text-4xl font-bold pt-24 max-sm:text-[1.7rem]">
         New collection
       </h1>
-      <div className="flex items-center gap-2 pt-16">
-        <div className="card w-2/6">
-          <Card className="rounded-lg">
-            <CardTitle className="text-xl p-8 font-bold">SHARK MAN</CardTitle>
-            <CardHeader>
-              <figure className="flex justify-center">
-                <Image
-                  src="/collection01.png"
-                  width={2000}
-                  height={2000}
-                  alt="Article Image"
-                  className="w-1/3 rounded-xl"
-                />
-              </figure>
-              <CardDescription></CardDescription>
-            </CardHeader>
-            <CardFooter className="flex justify-between py-12 font-bold">
-              $6.99
-              <Button
-                size="default"
-                className="rounded-xl text-md bg-[#FFD300] font-bold text-black"
-              >
-                Add to bag
-                <ShoppingCart />
-              </Button>
-            </CardFooter>
-          </Card>
+      <div className="pt-16">
+        <div className="grid grid-cols-3 gap-20 max-lg:gap-5 max-md:grid-cols-2 max-sm:grid-cols-1">
+          {CollectionData.map((lego, index) => {
+            return (
+              <Card className="rounded-lg" key={index}>
+                <CardTitle className="text-xl p-8 font-bold">
+                  {lego.title}
+                </CardTitle>
+                <CardHeader>
+                  <figure className="flex justify-center h-40">
+                    <Image
+                      src={lego.image}
+                      width={2000}
+                      height={2000}
+                      alt="Lego article"
+                      className="w-1/3 rounded-xl "
+                    />
+                  </figure>
+                  <CardDescription></CardDescription>
+                </CardHeader>
+                <CardFooter className="flex justify-between py-12 font-bold">
+                  ${lego.price}
+                  <Button
+                    size="default"
+                    className="rounded-xl text-md bg-[#FFD300] font-bold text-black max-md:text-sm"
+                  >
+                    Add to bag
+                    <ShoppingCart />
+                  </Button>
+                </CardFooter>
+              </Card>
+            );
+          })}
         </div>
       </div>
       <div className="glow -right-32 max-md:-right-40"></div>
