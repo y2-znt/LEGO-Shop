d92fa3d3cc479f58159b3d81f7c862b8527e2d6c
unknown <yonideserbaix@gmail.com>
2024-04-08 21:47:10 +0200

    feat: create cart component

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function page() {
+  return (
+    <div>
+      <Header />
+      <div className="max-w-6xl mx-auto max-xl:px-8">Cart page</div>
+    </div>
+  );
+}
