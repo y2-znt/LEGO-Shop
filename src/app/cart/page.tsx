9825673461a796bd85705e077e031da3cb2e43dc
unknown <yonideserbaix@gmail.com>
2024-04-09 20:53:51 +0200

    feat: add list

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,7 +5,17 @@ export default function page() {
   return (
     <div>
       <Header />
-      <div className="max-w-6xl mx-auto max-xl:px-8">Cart page</div>
+      <main className="max-w-6xl mx-auto max-xl:px-8">
+        <h1 className="text-3xl lg:text-4xl font-bold pt-24 max-sm:text-[1.7rem]">
+          Shopping Cart
+        </h1>
+        <ul className="flex flex-row justify-between mt-24 font-semibold">
+          <li>Product</li>
+          <li>Price</li>
+          <li>Quantity</li>
+          <li>Total</li>
+        </ul>
+      </main>
     </div>
   );
 }
