7bad98f8e6db019e748db509f1328b98e3698ece
unknown <yonideserbaix@gmail.com>
2024-04-20 12:11:20 +0200

    feat(Contact): create form

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,12 +1,56 @@
-import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Contact() {
   return (
     <div className="flex flex-col lg:gap-0 items-start pt-28">
       <div className="lg:text-left">
         <h1 className="text-3xl lg:text-4xl font-bold  max-lg:pt-0 max-sm:text-[1.7rem]">
-          Contacts
+          Contact
         </h1>
+      </div>
+      <div className="mt-12 min-h-[30vh] w-full rounded-xl flex justify-evenly max-sm:flex-col max-lg:items-center max-lg:pb-12">
+        <Image
+          src="/assets/contact-lego.png"
+          width={1000}
+          height={1000}
+          alt=""
+          className="relative w-1/6 -pr-2 rounded- max-lg:rounded-3xl max-sm:w-1/3 max-lg:mr-14"
+        ></Image>
+        <Card className="w-[450px] max-sm:w-full max-sm:mt-12">
+          <CardHeader>
+            <CardTitle className="font-bold">CONTACT US</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-2">
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" placeholder="Your name" />
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <Label htmlFor="framework">E-mail</Label>
+                  <Input id="name" placeholder="Your E-mail" />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full py-5 font-bold text-black mt-3 active:bg-amber-200 transition-all">
+              Confirm
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
