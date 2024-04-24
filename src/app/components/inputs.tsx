62e38b495dfd5968e24681d6f47e375bfc3a2ce9
unknown <yonideserbaix@gmail.com>
2024-04-24 23:32:57 +0200

    feat(inputs): init types

---
+++ src/app/components/inputs.tsx
@@ -0,0 +1,25 @@
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+type inputType = {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+};
+
+const Input = ({
+  id,
+  label,
+  type = "text",
+  disabled = false,
+  required = false,
+  register,
+  errors,
+}: inputType) => {
+  return <div></div>;
+};
+
+export default Input;
