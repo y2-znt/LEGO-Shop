279bfb837746079b9728288826b3cd1433f7530f
unknown <yonideserbaix@gmail.com>
2024-04-25 04:54:03 +0200

    feat(nextauth): implemented NextAuth with PrismaAdapter

--- src/lib/api/auth/[...nextauth].ts
+++ src/lib/api/auth/[...nextauth].ts
@@ -1,14 +1,62 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../../prisma/prismadb";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  // Use PrismaAdapter to manage user sessions with Prisma
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    // Configure custom credentials provider for email/password authentication
+    CredentialsProvider({
+      name: "credentials",
+      // Define credentials fields (email and password)
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+        },
+        password: {
+          label: "passowrd",
+          type: "passowrd",
+        },
+      },
+      // Authorize function to validate credentials
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Invalid email or password");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid email or password");
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error("Invalid email or password");
+        }
+        return user; // Return user if authenticated successfully
+      },
+    }),
   ],
+  pages: {
+    signIn: "/login", // Set login page
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
