fc5b29f053c1ce7dfb25e24bc6a18e3a8d478418
unknown <yonideserbaix@gmail.com>
2024-05-12 16:30:23 +0200

    feat(forms): add eye button to show password

--- src/components/ui/inputs.tsx
+++ src/components/ui/inputs.tsx
@@ -1,6 +1,8 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 type inputType = {
   id: string;
@@ -21,6 +23,7 @@ const Inputs: React.FC<inputType> = ({
   register,
   errors,
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="m-auto relative py-2 font">
       <Label htmlFor={id}>{label}</Label>
@@ -30,9 +33,18 @@ const Inputs: React.FC<inputType> = ({
         disabled={disabled}
         {...register(id, { required })}
         placeholder=""
-        type={type}
+        type={type === "password" && !showPassword ? "password" : "text"}
         className={`${errors[id] ? "border-red-500" : "border-slate-300"}`}
       />
+      {type === "password" && (
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)} // inverse the state
+          className="absolute bottom-[1.2rem] right-0 flex items-center pr-3 text-sm text-gray-500"
+        >
+          {showPassword ? <FaEyeSlash /> : <FaEye />}{" "}
+        </button>
+      )}
       {errors[id] && (
         <p className="text-xs font-semibold text-red-500 mt-1">
           This field is required
