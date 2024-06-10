3c87a62e6b6d57daf9b6e5173cc12b6013600c93
unknown <yonideserbaix@gmail.com>
2024-06-10 15:26:42 +0200

    feat(addProduct): conditional rendering if user is not admin

--- src/app/admin/(adminPages)/add-products/page.tsx
+++ src/app/admin/(adminPages)/add-products/page.tsx
@@ -1,4 +1,14 @@
-export default function AddProducts() {
+import AccesDenied from "@/components/ui/AccesDenied";
+import { getCurrentUser } from "@/pages/api/auth/getCurrentUser";
+
+export default async function AddProducts() {
+  const currentUser = await getCurrentUser();
+
+  // Check if the current user is not authenticated or is not an admin
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <AccesDenied title="Oops! Acces denied" />;
+  }
+
   return (
     <div className="max-w-6xl mx-auto max-xl:px-8 font-bold text-black">
       <p className="text-3xl lg:text-4xl pt-10 max-sm:text-[1.7rem]">
