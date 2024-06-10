5916e57c958800882afe0148875e7b28f7d46a28
unknown <yonideserbaix@gmail.com>
2024-06-10 11:38:55 +0200

    feat(admin): create admin navbar item

---
+++ src/app/components/admin/AdminNavItem.tsx
@@ -0,0 +1,11 @@
+import { IconType } from "react-icons/lib";
+
+type AdminNavItemType = {
+  selected?: boolean;
+  icon?: IconType;
+  label?: string;
+};
+
+export default function AdminNavItem() {
+  return <div>AdminNavItem</div>;
+}
