1716a9d577c33bb4ecbb27f994c013e48847f427
unknown <yonideserbaix@gmail.com>
2024-06-13 16:01:52 +0200

    feat(collection): render products in home page

--- src/app/components/Collection.tsx
+++ src/app/components/Collection.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Button } from "@/components/ui/shadcn/button";
 import {
   Card,
@@ -9,86 +10,91 @@ import {
 } from "@/components/ui/shadcn/card";
 import { addToCart } from "@/lib/redux/features/cartSlice";
 import { addToFav, removeFromFav } from "@/lib/redux/features/favSlice";
+import { Product } from "@prisma/client";
 import Image from "next/image";
 import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
 import { IoBag } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
-import { Data } from "./Data";
 
-export default function Collection() {
-  const { products } = Data;
+type CollectionType = {
+  products: Product[];
+};
 
+export default function Collection({ products }: CollectionType) {
   const favItems = useSelector((state: any) => state.favorite.favItems);
   const dispatch = useDispatch();
 
-  const handleAddToCart = (lego: any) => {
-    dispatch(addToCart(lego));
+  const handleAddToCart = (product: Product) => {
+    dispatch(addToCart(product));
   };
 
-  const toggleFavorite = (lego: any) => {
-    const isAlreadyFavorite = favItems.some((item: any) => item.id === lego.id);
+  const toggleFavorite = (product: Product) => {
+    const isAlreadyFavorite = favItems.some(
+      (item: any) => item.id === product.id
+    );
     if (isAlreadyFavorite) {
-      dispatch(removeFromFav(lego));
+      dispatch(removeFromFav(product));
     } else {
-      dispatch(addToFav(lego));
+      dispatch(addToFav(product));
     }
   };
 
   return (
-    <main>
+    <div>
       <h1 className="text-3xl lg:text-4xl font-bold pt-24 max-sm:text-[1.7rem]">
-        New collection
+        New Collection
       </h1>
       <div className="pt-16">
         <div className="grid grid-cols-3 gap-20 max-lg:gap-5 max-md:grid-cols-2 max-sm:grid-cols-1">
-          {products.map((lego, index) => {
-            const isFavorite = favItems.some(
-              (item: any) => item.id === lego.id
-            );
-            return (
-              <Card className="rounded-lg" key={index}>
-                <CardTitle className="flex justify-between text-xl p-8 font-bold">
-                  {lego.title}
-                  <span
-                    className="cursor-pointer"
-                    onClick={() => toggleFavorite(lego)}
-                  >
-                    {isFavorite ? (
-                      <IoIosHeart color="red" size={25} />
-                    ) : (
-                      <IoIosHeartEmpty color="gray" size={25} />
-                    )}
-                  </span>
-                </CardTitle>
-                <CardHeader>
+          {products &&
+            products.map((product) => {
+              const isFavorite = favItems.some(
+                (item: any) => item.id === product.id
+              );
+              return (
+                <Card className="rounded-lg" key={product.id}>
+                  <CardHeader className="flex justify-between p-8">
+                    <CardTitle className="text-xl font-bold">
+                      {product.name}
+                    </CardTitle>
+                    <span
+                      className="cursor-pointer"
+                      onClick={() => toggleFavorite(product)}
+                    >
+                      {isFavorite ? (
+                        <IoIosHeart color="red" size={25} />
+                      ) : (
+                        <IoIosHeartEmpty color="gray" size={25} />
+                      )}
+                    </span>
+                  </CardHeader>
                   <figure className="flex justify-center h-40">
                     <Image
-                      src={lego.image}
+                      src={product.image}
                       width={2000}
                       height={2000}
-                      alt="Lego article"
-                      className="w-1/3 rounded-xl "
+                      alt={product.name}
+                      className="w-1/3 rounded-xl"
                     />
                   </figure>
                   <CardDescription></CardDescription>
-                </CardHeader>
-                <CardFooter className="flex justify-between py-12 font-bold">
-                  ${lego.price}
-                  <Button
-                    onClick={() => handleAddToCart(lego)}
-                    size="default"
-                    className="rounded-xl text-md  max-md:text-sm "
-                  >
-                    Add to cart
-                    <IoBag size={25} className="pl-2" />
-                  </Button>
-                </CardFooter>
-              </Card>
-            );
-          })}
+                  <CardFooter className="flex justify-between py-12 font-bold">
+                    ${product.price}
+                    <Button
+                      onClick={() => handleAddToCart(product)}
+                      size="default"
+                      className="rounded-xl text-md max-md:text-sm"
+                    >
+                      Add to cart
+                      <IoBag size={25} className="pl-2" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              );
+            })}
         </div>
       </div>
       <div className="glow -right-32 max-md:-right-40"></div>
-    </main>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+import getProducts from "@/pages/api/auth/getProducts";
 import Collection from "./components/Collection";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getProducts();
+
   return (
     <div>
       <div className="max-w-6xl mx-auto max-xl:px-8  text-black">
         <Hero />
-        <Collection />
+        <Collection products={products} />
         <Contact />
       </div>
       <Footer />
