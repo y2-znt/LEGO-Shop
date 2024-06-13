8744d4c452cd5baa3f768c6b028b1bdf9e32ac0a
unknown <yonideserbaix@gmail.com>
2024-06-13 15:48:50 +0200

    feat(manageProduct): display products in table

--- src/app/admin/(adminPages)/manage-products/ManageProductsClient.tsx
+++ src/app/admin/(adminPages)/manage-products/ManageProductsClient.tsx
@@ -1,10 +1,21 @@
 "use client";
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/shadcn/table";
 import { Product } from "@prisma/client";
+import Image from "next/image";
 
 type ManageProductsClientType = {
   products: Product[];
 };
+
 export default function ManageProductsClient({
   products,
 }: ManageProductsClientType) {
@@ -13,6 +24,37 @@ export default function ManageProductsClient({
       <h1 className="text-3xl lg:text-4xl pt-10 max-sm:text-[1.7rem]">
         Manage Products
       </h1>
+      <Table className="mt-10">
+        <TableCaption>A list of your products.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>In Stock</TableHead>
+            <TableHead>Image</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>
+                {product.inStock ? "in stock" : "out of stock"}
+              </TableCell>
+              <TableCell>
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  className="w-10 h-10 object-cover"
+                  width={50}
+                  height={50}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
