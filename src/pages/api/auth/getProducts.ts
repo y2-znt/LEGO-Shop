69f863487f18bea23d28011b2c78887cd3aa5410
unknown <yonideserbaix@gmail.com>
2024-06-14 22:47:37 +0200

    feat(getProducts): select elements with order by filter

--- src/pages/api/auth/getProducts.ts
+++ src/pages/api/auth/getProducts.ts
@@ -1,6 +1,8 @@
 import prisma from "../../../../prisma/prismadb";
 
 export default async function getProducts() {
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany({
+    orderBy: [{ inStock: "desc" }, { createdAt: "asc" }],
+  });
   return products;
 }
