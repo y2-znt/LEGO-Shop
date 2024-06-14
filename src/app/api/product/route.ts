8ed4808a4fb77267c63e051f5cce65173f519300
unknown <yonideserbaix@gmail.com>
2024-06-14 12:37:52 +0200

    feat(prisma api): create the update function

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -26,3 +26,24 @@ export async function POST(req: Request) {
   // Return a JSON response with the newly created user object
   return NextResponse.json(product);
 }
+
+export async function PUT(req: Request) {
+  const currentUser = await getCurrentUser();
+
+  // Check if the current user is not authenticated or is not an admin
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return NextResponse.error();
+  }
+
+  const body = await req.json();
+  const { id, inStock } = body;
+
+  // Update the status of the product (stock or out of stock)
+  const product = await prisma.product.update({
+    where: { id: id },
+    data: { inStock: inStock },
+  });
+
+  // Return a JSON response with the newly created user object
+  return NextResponse.json(product);
+}
