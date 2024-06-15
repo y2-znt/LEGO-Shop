905d5ed1c887600d644609ef23dc3f4b828c3b05
y2_dev <yonideserbaix@gmail.com>
2024-06-15 17:45:23 +0200

    feat(Summary): create table

--- src/app/admin/Summary.tsx
+++ src/app/admin/Summary.tsx
@@ -1,5 +1,14 @@
 "use client";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/shadcn/table";
 import { Product } from "@prisma/client";
+import Image from "next/image";
 
 type SummaryType = {
   products: Product[];
@@ -26,6 +35,42 @@ export default function Summary({ products }: SummaryType) {
         <div className="p-4 border rounded-lg">
           <h2 className="text-xl">Out of Stock</h2>
           <p>{outOfStockCount}</p>
+        </div>
+      </div>
+      <div className="mt-20">
+        <h2 className="text-2xl">Recently Added Products</h2>
+        <div className="overflow-auto ">
+          <Table className="mt-10">
+            <TableHeader>
+              <TableRow>
+                <TableHead>Image</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Updated At</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {products.slice(0, 5).map((product, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Image
+                      src={product.image}
+                      alt={product.name}
+                      className="w-12 h-16 rounded-lg border p-1 object-cover"
+                      width={50}
+                      height={50}
+                    />
+                  </TableCell>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>$ {product.price}</TableCell>
+                  <TableCell>
+                    {new Date(product.updatedAt).toLocaleDateString()} -
+                    {new Date(product.updatedAt).toLocaleTimeString()}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </div>
