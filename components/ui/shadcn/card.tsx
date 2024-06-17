16e468580af511dfc1a0c40675103373bec7eb1f
y2_dev <yonideserbaix@gmail.com>
2024-06-17 17:45:20 +0200

    fix(shadcn): move utils.ts

--- components/ui/shadcn/card.tsx
+++ components/ui/shadcn/card.tsx
@@ -1,7 +1,7 @@
 "use client";
 import * as React from "react";
 
-import { cn } from "../../../lib/utils";
+import { cn } from "./utils";
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -76,9 +76,9 @@ CardFooter.displayName = "CardFooter";
 
 export {
   Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
   CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 };
