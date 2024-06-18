843bbed4a5f14e3d344fea89a4b11cc0fa27230f
y2_dev <yonideserbaix@gmail.com>
2024-06-18 18:26:17 +0200

    feat(route-api): PUT function to put user name

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -32,3 +32,24 @@ export async function DELETE(
     return NextResponse.error();
   }
 }
+
+export async function PUT(req: Request) {
+  const currentUser = await getCurrentUser();
+
+  // Check if the current user is not authenticated or is not an admin
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return NextResponse.error();
+  }
+
+  const body = await req.json();
+  const { id, name } = body;
+
+  // Update the name of the user (stock or out of stock)
+  const user = await prisma.product.update({
+    where: { id: id },
+    data: { name: name },
+  });
+
+  // Return a JSON response with the newly created user object
+  return NextResponse.json(user);
+}
