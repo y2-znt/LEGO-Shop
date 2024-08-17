5c9185c3b63cc3a2068e6ea28e22c9a114d7ead7
y2_dev <yonideserbaix@gmail.com>
2024-08-17 16:42:13 +0200

    feat(): init API route for creat payment intent

---
+++ app/api/create-payment-intent/route.ts
@@ -0,0 +1,85 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+import prisma from "../../../prisma/prismadb";
+import { getCurrentUser } from "../../../pages/api/auth/getCurrentUser";
+import { CartType } from "../../redux/features/cartSlice";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-06-20",
+});
+
+const calculateOrderAmount = (items: CartType[]) => {
+  const totalPrice = items.reduce((acc, item) => {
+    const itemTotal = item.cartTotalAmount * item.cartTotalQuantity;
+    return acc + itemTotal;
+  }, 0);
+  return totalPrice;
+};
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const body = await request.json();
+  const { items, payment_intent_id } = body;
+  const total = calculateOrderAmount(items) * 100;
+  const orderData = {
+    user: { connect: { id: currentUser.id } },
+    amount: total,
+    currency: "usd",
+    status: "pending",
+    deliveryStatus: "pending",
+    paymentIntentId: payment_intent_id,
+    products: items,
+  };
+
+  if (payment_intent_id) {
+    const current_intent =
+      await stripe.paymentIntents.retrieve(payment_intent_id);
+
+    if (current_intent) {
+      const updated_intent = await stripe.paymentIntents.update(
+        payment_intent_id,
+        { amount: total },
+      );
+
+      // update the order
+
+      const [existing_order, update_order] = await Promise.all([
+        prisma.order.findFirst({
+          where: { paymentIntentId: payment_intent_id },
+        }),
+        prisma.order.update({
+          where: { paymentIntentId: payment_intent_id },
+          data: {
+            amount: total,
+            products: items,
+          },
+        }),
+      ]);
+
+      if (!existing_order) {
+        return NextResponse.json(
+          { error: "Invalid Payment Intent" },
+          { status: 400 },
+        );
+      }
+      return NextResponse.json({ paymentIntent: updated_intent });
+    }
+  } else {
+    // create the payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    });
+    // create the order
+    orderData.payment_intent_id = paymentIntent.id;
+
+    await prisma.order.create({ data: orderData });
+    return NextResponse.json({ paymentIntent });
+  }
+}
--- app/redux/features/cartSlice.ts
+++ app/redux/features/cartSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "sonner";
 
-type CartType = {
+export type CartType = {
   cartItems: any[];
   cartTotalQuantity: number;
   cartTotalAmount: number;
