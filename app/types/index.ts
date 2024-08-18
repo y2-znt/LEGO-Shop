1206437dfff73b225129f61e80ffaf7849f61b7d
y2_dev <yonideserbaix@gmail.com>
2024-08-19 00:10:13 +0200

    fix(types): prettier ignore

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,10 @@
-export type SafeUser = {
+import { User } from "@prisma/client";
+
+// prettier-ignore
+export type SafeUser = Omit<
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
