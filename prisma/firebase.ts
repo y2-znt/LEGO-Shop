7e531a821d9f8de3a35e4d3929ef2616ab170891
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-11-01 23:35:43 +0100

    .

--- prisma/firebase.ts
+++ prisma/firebase.ts
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGlDYAah0sDkACe9Gb2qcBWs3jhHzXOd4",
-  authDomain: "lego-ecom.firebaseapp.com",
-  projectId: "lego-ecom",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: "lego-ecom.appspot.com",
-  messagingSenderId: "526671944231",
-  appId: "1:526671944231:web:2c56607d6524739b418c02",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 export default firebaseApp;
