f3beef71d6f5a6cc8626d8522bd6390dd7590740
y2_znt <yonideserbaix@gmail.com>
2025-05-02 13:51:44 +0200

    feat: add useFavoriteActions hook for managing favorite items and user notifications

---
+++ hooks/useFavoriteActions.tsx
@@ -0,0 +1,18 @@
+import { FavoriteItem, useFavoriteStore } from "@/stores/FavoriteStore";
+import { Product } from "@prisma/client";
+import { toast } from "sonner";
+
+export const useFavoriteActions = () => {
+  const { addToFavorites, removeFromFavorites } = useFavoriteStore();
+
+  return {
+    add: (product: Product) => {
+      addToFavorites({ ...product, quantity: 1 });
+      toast.success(`${product.name} added to favorites`);
+    },
+    remove: (item: Product | FavoriteItem) => {
+      removeFromFavorites(item.id);
+      toast.warning(`${item.name} removed from favorites`);
+    },
+  };
+};
